c6ae85495386b9718c2e7d45321c5f550a1316ab
Josh Varney <joshcainvarney@gmail.com>
2018-07-11 10:37:23 -0400

    works with 2 pennies

--- coin_changer2.rb
+++ coin_changer2.rb
@@ -1,3 +1,11 @@
 def coin_changer(amount)
-	p amount
+	amount
+	coins = ["penny", "pennies"]
+	if amount == 1
+		coins = coins[0]
+	elsif amount < 5
+		coins = amount.to_s + " " + coins[1]
+	end 
+	coins
+
 end
--- coin_changer2_test.rb
+++ coin_changer2_test.rb
@@ -2,9 +2,17 @@ require "minitest/autorun"
 require_relative "coin_changer2.rb"
 class Test_Coin_changer2 <Minitest::Test
 
-	def test_that_can_accept_integer_as_argument
+	# def test_that_can_accept_integer_as_argument
+	# 	amount = 1
+	# 	assert_equal(Integer, coin_changer(amount).class)
+	# end
+	def test_that_1_is_a_penny
 		amount = 1
-		assert_equal(Integer, coin_changer(amount).class)
+		assert_equal("penny", coin_changer(amount))
+	end
+	def test_that_2_will_come_back_as_2_pennies
+		amount = 2
+		assert_equal("2 pennies", coin_changer(amount))
 	end
 
 
