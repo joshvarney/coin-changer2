8e9daa090af7693952d8c3af878a3c84055e3710
Josh Varney <joshcainvarney@gmail.com>
2018-07-11 15:22:36 -0400

    coin changer refactored and all tests still pass

--- coin_changer2.rb
+++ coin_changer2.rb
@@ -1,5 +1,4 @@
 def coin_changer(amount)
-	amount
 	exchange = {}
 	coins = {
 			quarter: 25,
@@ -7,45 +6,11 @@ def coin_changer(amount)
 			nickel: 5,
 			penny: 1,
 		}
-		counter = 0
 		coins.each do  |key, value|
-
 			if amount >= value
 				exchange[key] = amount / value
 				amount = amount % value
-				amount 
-			# elsif amount > value && amount < 10
-			# 	amount = amount / value
-			# 	p amount
-			# 	exchange[key] = amount
-			# 	p amount = amount % value
-
-			
+			end
 		end
-		end
-
-
-			# amount2 = amount / value
-			# p amount2
-			# if amount != value
-
-			# 	exchange[key] = amount % amount
-			# 	exchange
-			# elsif amount == value
-			# 		exchange[key] = 1
-			# 		exchange
-				# elsif
-				# amount2 = amount % 5
-				# exchange = amount1.to_s + " "
-			 
-				
-			 
-			
-			# elsif amount < 5
-	 	# 		exchange = amount.to_s + " " + coins[2..4].to_s
-			# elsif amount == 5
-			# 	coins2 = "1" + " " + coins[amount]
-			# elsif amount < 10 && amount > 5
-			# 	coins2 = "1"
 	exchange
 end
